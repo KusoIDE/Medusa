a0a9b4d4648f63270c1be1bb71fc07d8a6de3a41
Sameer Rahmani <lxsameer@gnu.org>
2014-11-20 23:35:04 +0330

    Stupid mistake in package model fixed

--- app/models/package.rb
+++ app/models/package.rb
@@ -1,11 +1,12 @@
+# This model represent an Emacs elpa compatible package
 class Package
 
   include Mongoid::Document
   include Mongoid::Timestamp
 
   field :name, type: String
-  field :home_page, type: Url
-  field :documents, type: Url
+  field :home_page, type: String
+  field :documents, type: String
   field :versions, type: List
   field :dependencies, type: Hash
 end
