41b7ffada6efd7ce6d32790521c7833aee79439b
Sameer Rahmani <lxsameer@gnu.org>
2014-11-27 00:57:21 +0330

    Couple of changes into archive contents

--- app/controllers/api/v1/packages_controller.rb
+++ app/controllers/api/v1/packages_controller.rb
@@ -1,13 +1,13 @@
 class Api::V1::PackagesController < ApplicationController
 
-  respond_to :plain
+  respond_to :html, :json
 
   def archive_contents
     @packages = Package.first
-    respond_with @packages
-    #respond_to do |format|
-      #format.plain { render :archive_contents}
-    #end
+    #respond_with @packages
+    respond_to do |format|
+      format.html { render template: 'api/v1/packages/archive_contents' }
+    end
   end
 
   def index
