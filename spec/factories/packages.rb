3293e63ac18f10ad91a293b47a7f848c3f491c3f
Sameer Rahmani <lxsameer@gnu.org>
2015-01-09 14:43:00 +0330

    success test added to package spec

--- spec/factories/packages.rb
+++ spec/factories/packages.rb
@@ -6,6 +6,13 @@ PKG = ActionDispatch::Http::UploadedFile.new(tempfile: pkg_file,
 FactoryGirl.define do
   factory :package do
     name Faker::App.name
+    versions({ '1.0.0' => 0 })
+    description Faker::Lorem.paragraph
+    packages { { '1.0.0' => "#{name}/#{name}-#{versions.keys[0]}.tar" }  }
+  end
+
+  factory :package_params, :class => 'Package' do
+    name Faker::App.name
     version Faker::App.version
     description Faker::Lorem.paragraph
     package PKG
--- spec/requests/packages_spec.rb
+++ spec/requests/packages_spec.rb
@@ -16,14 +16,29 @@ RSpec.describe 'Package API' do
           post '/api/v1/packages', params
 
           expect(response.status).to eq(400)
-          expect(response.body).to have_node(:errors)
+          expect(response.body).to have_node(:errors).including_text(field.to_s)
         end
+      end
+
+      it 'saves the package successfully in database' do
+        params = attributes_for(:package_params)
+
+        post '/api/v1/packages', params
+
+        expect(response.status).to eq(200)
       end
 
       it 'respond with 409 (conflict) if same package exists' do
         package = create(:package)
-        params = package.attributes_for
 
+        params = attributes_for(:package_params) do |p|
+          p[:name] = package.name
+          p[:version] = package.versions.keys[0]
+        end
+
+        post '/api/v1/packages', params
+        expect(response.status).to eq(409)
+        expect(response.body).to have_node(:errors).includeing_text('already exists')
       end
 
     end
