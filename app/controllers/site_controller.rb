7b0ea82d58370d2d16e8e1519b970f385dc21232
ehsan mahmoudi <e.mahmoudi98@gmail.com>
2015-03-28 23:29:54 +0000

    packages view and set a counter for index

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,5 +1,10 @@
 class SiteController < ApplicationController
   def index
+    packages = Package.all
+    @counter = 0
+    packages.each do |package|
+      @counter += package.downloads
+    end
   end
 
   def packages
